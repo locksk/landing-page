f6c0f567b95ef552019dc67d95eb0c7cecd629e0
Siobhan <locksk@cardiff.ac.uk>
2023-08-14 16:10:50 +0100

    new vibe

--- index.js
+++ index.js
@@ -1,59 +1,28 @@
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('body,html').animate({
+      scrollTop: $(hash).offset().top
+      }, 1200, function(){
+      window.location.hash = hash;
+     });
+     } 
+    });
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+var width = $(window).width(); 
+
+window.onscroll = function(){
+if ((width >= 900)){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $("#middle").css("background-size","150% auto");
+    }else{
+        $("#middle").css("background-size","100% auto");        
     }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
 }
-
-function magnify(imglink){
-    $("#img_here").css("background",`url('${imglink}') center center`);
-    $("#magnify").css("display","flex");
-    $("#magnify").addClass("animated fadeIn");
-    setTimeout(function(){
-        $("#magnify").removeClass("animated fadeIn");
-    },800);
-}
-
-function closemagnify(){
-    $("#magnify").addClass("animated fadeOut");
-    setTimeout(function(){
-        $("#magnify").css("display","none");
-        $("#magnify").removeClass("animated fadeOut");
-        $("#img_here").css("background",`url('') center center`);
-    },800);
-}
+};
 
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
@@ -61,29 +30,16 @@ setTimeout(function(){
       $("#loading").removeClass("animated fadeOut");
       $("#loading").css("display","none");
     },800);
-},1650);
+},1450);
 
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('body,html').animate({
-        scrollTop: $(hash).offset().top
-        }, 1800, function(){
-        window.location.hash = hash;
-       });
-       } 
-      });
-  });
-  
-  var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 var rootElement = document.documentElement;
 
 function handleScroll() {
   // Do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if (rootElement.scrollTop / scrollTotal > 0.04) {
+  if (rootElement.scrollTop / scrollTotal > 0.18) {
     // Show button
     scrollToTopBtn.classList.add("showBtn");
   } else {
@@ -101,3 +57,43 @@ function scrollToTop() {
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
 document.addEventListener("scroll", handleScroll);
+
+function magnify(imglink){
+  $("#img_here").css("background",`url('${imglink}') center center`);
+  $("#magnify").css("display","flex");
+  $("#magnify").addClass("animated fadeIn");
+  setTimeout(function(){
+      $("#magnify").removeClass("animated fadeIn");
+  },800);
+}
+
+function closemagnify(){
+  $("#magnify").addClass("animated fadeOut");
+  setTimeout(function(){
+      $("#magnify").css("display","none");
+      $("#magnify").removeClass("animated fadeOut");
+      $("#img_here").css("background",`url('') center center`);
+  },800);
+}
+
+setTimeout(function(){
+  $("#loading").addClass("animated fadeOut");
+  setTimeout(function(){
+    $("#loading").removeClass("animated fadeOut");
+    $("#loading").css("display","none");
+  },800);
+},1650);
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('body,html').animate({
+      scrollTop: $(hash).offset().top
+      }, 1800, function(){
+      window.location.hash = hash;
+     });
+     } 
+    });
+});
